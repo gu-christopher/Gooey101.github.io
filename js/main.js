1da218469dd9ef10fc90966a4571497f3e3f912b
Christopher Gu <christophergu@MacBook-Pro-8.local>
2020-09-15 01:22:10 -0500

    new website design

---
+++ js/main.js
@@ -0,0 +1,121 @@
+function adjustFontSizes() {
+    var centerPageBox = $('.center-page-box');
+
+    if($(window).width() > 814) {
+        centerPageBox.css('top', $(window).height() / 2 - $('.center-page-box').height() / 2);
+    } else {
+        centerPageBox.css('top', '');
+    }
+}
+
+function adjustBoxWidths() {
+    if(window.localStorage.getItem('activePage') === '#education') {
+        $('.extracurricular-grid').css('width', $('#education .large-box').innerWidth() + 'px');
+    }
+}
+
+function setupCenterBoxes() {
+    var activePage = window.localStorage.getItem('activePage');
+
+    if(!activePage) {
+        activePage = '#about';
+    }
+
+    $('.center-page-box').each(function(index, value) {
+        if($(this).is($(activePage))) {
+            $(this).css('left', '50%');
+        }
+    });
+}
+
+function goToDataLink() {
+    window.open($(this).data('link'), '_blank');
+}
+
+$(document).ready(() => {
+    adjustFontSizes();
+    adjustBoxWidths();
+    setupCenterBoxes();
+
+    var activePage = window.localStorage.getItem('activePage');
+
+    if(!activePage) {
+        activePage = '#about';
+    }
+
+    $('a[href="' + activePage + '"]').click();
+});
+
+$(window).resize(() => {
+    adjustFontSizes();
+    adjustBoxWidths();
+
+    if($(window).width() > 991) {
+        $('.navbar').removeClass('expanded');
+        $('.navbar-collapse').removeClass('expanded');
+    } else if($('.navbar-collapse').hasClass('show')) {
+        $('.navbar').addClass('expanded');
+        $('.navbar-collapse').addClass('expanded');
+    }
+});
+
+$('.navbar-collapse').on('show.bs.collapse', () => {
+    $('.navbar').addClass('expanded');
+});
+
+$('.navbar-collapse').on('shown.bs.collapse', () => {
+    $('.navbar-collapse').addClass('expanded');
+});
+
+$('.navbar-collapse').on('hide.bs.collapse', () => {
+    $('.navbar').removeClass('expanded');
+    $('.navbar-collapse').removeClass('expanded');    
+});
+
+$('.navbar-nav > li > a').click(() => {
+    $('.navbar-collapse').collapse('hide');
+});
+
+$('.navbar-brand').click(() => {
+    $('.navbar-collapse').collapse('hide');
+});
+
+$('.navbar a').click(function(e) {
+    e.preventDefault();
+
+    var prevActivePage = window.localStorage.getItem('activePage');
+
+    window.localStorage.setItem('activePage', $(this)[0].hash);
+
+    var activePage = window.localStorage.getItem('activePage');
+
+    $('.navbar a.active').removeClass('active');
+    $(this).addClass('active');
+
+    adjustBoxWidths();
+
+    if(prevActivePage !== activePage) {
+        if($('.center-page-box').index($(prevActivePage)) > $('.center-page-box').index($(activePage))) {
+            $(activePage).css('left', '-150%');
+            $(prevActivePage).animate({
+                left: '150%',
+                opacity: 0
+            }, 500);
+        } else {
+            $(activePage).css('left', '150%');
+            $(prevActivePage).animate({
+                left: '-150%',
+                opacity: 0
+            }, 500);
+        }
+
+        $(activePage).animate({
+            left: '50%',
+            opacity: 1
+        }, 500);
+    }
+    
+});
+
+$('#press .press-grid .large-box').click(goToDataLink);
+$('#portfolio .portfolio-grid .portfolio-item').click(goToDataLink);
